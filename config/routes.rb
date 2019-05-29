aef08a326a9c687bcd02c59c3c67d5ee3a452ace
Richy <ec2-user@ip-172-31-83-98.ec2.internal>
2019-05-29 16:34:24 +0000

    Added user w/limited func,static pages, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get '/admin', to: 'users#admin_login'
+  get '/logout', to: 'users#logout'
   resources :doctors
   resources :patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_144734) do
+ActiveRecord::Schema.define(version: 2019_05_29_161412) do
 
   create_table "doctors", force: :cascade do |t|
     t.boolean "access_level"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2019_05_29_144734) do
     t.string "email"
     t.string "contact_number"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
