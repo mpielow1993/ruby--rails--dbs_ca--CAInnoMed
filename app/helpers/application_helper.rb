8a8c93df8d5987b78f552f856aa615c902941fb6
Emily Bowe <ebowe@tcd.ie>
2019-06-14 21:27:39 +0000

    Fix merge problem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,3 @@
 module ApplicationHelper
-<<<<<<< HEAD
-    
-    def resource_name
-    :user
-    end
 
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-  
-=======
->>>>>>> 5b1db0f... intial commit
 end
